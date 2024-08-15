f82e0dbb087fe9e78b2fb2ef4e6c7e0f76878c6b
Mostary Jahan <mostaryjahan01@gmail.com>
2024-08-15 12:15:10 +0600

    added card

--- src/home/Card/Card.jsx
+++ src/home/Card/Card.jsx
@@ -1,8 +1,13 @@
 
-const Card = () => {
+const Card = ({ product }) => {
     return (
-        <div>
-            
+        <div style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
+            <img src={product.productImage} alt={product.productName} style={{ width: '200px', height: '200px' }} />
+            <h3>{product.productName}</h3>
+            <p>{product.description}</p>
+            <p><strong>Price:</strong> ${product.price}</p>
+            <p><strong>Rating:</strong> {product.ratings} / 5</p>
+            <p><strong>Category:</strong> {product.category}</p>
         </div>
     );
 };
--- src/home/home/Home.jsx
+++ src/home/home/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Card from '../Card/Card'; 
 
 const Home = () => {
     const [products, setProducts] = useState([]);
@@ -10,7 +11,6 @@ const Home = () => {
                 const response = await axios.get('http://localhost:5000/products');
                 setProducts(response.data);
                 console.log('Fetched Products:', response.data); 
-
             } catch (error) {
                 console.error('Error fetching products:', error);
             }
@@ -22,17 +22,10 @@ const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
-            <div className='grid grid-cols-1 lg:grid cols-3'>
+            <div className='grid grid-cols-1 lg:grid-cols-3'>
                 {products.length > 0 ? (
                     products.map((product) => (
-                        <div key={product._id} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
-                            <img src={product.productImage} alt={product.productName} style={{ width: '200px', height: '200px' }} />
-                            <h3>{product.productName}</h3>
-                            <p>{product.description}</p>
-                            <p><strong>Price:</strong> ${product.price}</p>
-                            <p><strong>Rating:</strong> {product.ratings} / 5</p>
-                            <p><strong>Category:</strong> {product.category}</p>
-                        </div>
+                        <Card key={product._id} product={product} />
                     ))
                 ) : (
                     <p>Loading products...</p>
