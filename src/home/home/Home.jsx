1f57a0ea6b69bd4b0f104ceff4c1cdfbeb1cb2ba
Mostary Jahan <mostaryjahan01@gmail.com>
2024-08-15 12:07:05 +0600

    fetched products

--- src/home/home/Home.jsx
+++ src/home/home/Home.jsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/products');
+                setProducts(response.data);
+                console.log('Fetched Products:', response.data); 
+
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
     return (
         <div>
-            <h1>home page</h1>
+            <h1>Home Page</h1>
+            <div className='grid grid-cols-1 lg:grid cols-3'>
+                {products.length > 0 ? (
+                    products.map((product) => (
+                        <div key={product._id} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
+                            <img src={product.productImage} alt={product.productName} style={{ width: '200px', height: '200px' }} />
+                            <h3>{product.productName}</h3>
+                            <p>{product.description}</p>
+                            <p><strong>Price:</strong> ${product.price}</p>
+                            <p><strong>Rating:</strong> {product.ratings} / 5</p>
+                            <p><strong>Category:</strong> {product.category}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>Loading products...</p>
+                )}
+            </div>
         </div>
     );
 };
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -29,20 +29,16 @@ const SignUp = () => {
                 userUpdateProfile(name, photo)
                     .then(() => {
                         console.log("photo update");
-                        // const userInfo = {
-                        //     name: name,
-                        //     email: email,
-                        // }
-                        // axiosPublic.post('/users', userInfo)
-                        //     .then(res => {
-                        //         if (res.data.insertedId) {
-                        //             Swal.fire({
-                        //                 title: "Good job!",
-                        //                 text: "login successfully!",
-                        //                 icon: "success"
-                        //             });
-                        //         }
-                        //     })
+                        const userInfo = {
+                            name: name,
+                            email: email,
+                        }
+                        axiosPublic.post('/users', userInfo)
+                            .then(res => {
+                                if (res.data.insertedId) {
+                                   console.log('inserted id');
+                                }
+                            })
                     })
                     .catch(error => console.error(error))
                 navigate('/')
@@ -58,8 +54,7 @@ const SignUp = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Register now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold">SignUp now!</h1>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form onSubmit={handleSubmit(handleSignUp)} className="card-body">
